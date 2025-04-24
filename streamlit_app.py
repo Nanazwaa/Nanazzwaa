983ce686c78811323dce554c2d1d9adf7615fa1f
Nanazwaa <nazwaaurad@gmail.com>
2025-04-24 13:20:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("😆💞❤ Hiii wllcome nanazwarnas")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "20-04-2021)."
 )
