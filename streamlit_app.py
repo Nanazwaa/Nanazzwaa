c4c182e593802ad41d0a50ee40a71e1308c1a16d
Nanazwaa <nazwaaurad@gmail.com>
2025-05-22 11:33:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,10 @@ st.write(
 st.image("IMG_6012.jpeg",width=150)
 st.write("\n")
 st.subheader("Nanazwa Aura D")
-st.write("Mari Belajar Bersama  Yesz!")
+st.write("Mari Belajar Bersama  Yesz!") 
+#with col 1:
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:",value=0,step=1)
+if(angka % 2) == 0 : 
+    st.write(f"{angka}adalah bilangan genap") else:
+    st.write(f"{angka}adalah bilangan ganjil")
